a3c57bed8cdd0e2f0edddefd7b25b687ab4086d0
abigail85 <aidecj@gmail.com>
2023-10-18 07:17:09 -0500

    se adiciona la finalizacion de la automatización

---
+++ src/test/java/com/amazon/utils/MensajesFinales.java
@@ -0,0 +1,11 @@
+package com.amazon.utils;
+
+public class MensajesFinales {
+    public static final String MENSAJE_CREACION_FALLIDA= "No se ve el mensaje de bienvenida";
+    public static final String MENSAJE_CREACION_EXITOSA= "Welcome to the world's largest community of freelance software testers!";
+
+
+
+    private void MensajesFinales() {
+    }
+}
